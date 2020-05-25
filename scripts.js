9f38ddbcc2b56b261e083a66dab3a746985ff2d7
Lenito90 <59535265+Lenito90@users.noreply.github.com>
2020-05-25 13:10:04 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,52 @@
+/* Toggling experience */
+document.getElementById('design-experience-toggle')
+    .addEventListener('click', toggleDesignPanel);
+
+
+function toggleDesignPanel() {
+    let currentPanel = document.getElementById('design-experience-panel');
+    if (currentPanel.classList.contains('closed')) {
+        currentPanel.classList.remove('closed');
+    } else {
+        currentPanel.classList.add('closed');
+    }
+}
+
+document.getElementById('personal-experience-toggle')
+    .addEventListener('click', togglePersonalPanel);
+
+
+function togglePersonalPanel() {
+    let currentPanel = document.getElementById('personal-experience-panel');
+    if (currentPanel.classList.contains('closed')) {
+        currentPanel.classList.remove('closed');
+    } else {
+        currentPanel.classList.add('closed');
+    }
+}
+
+document.getElementById('design-elevated-button')
+    .addEventListener('click', toggleDesignButton);
+
+function toggleDesignButton() {
+    let imageInput = document.getElementById('design-elevated-button');
+    if (imageInput.src.indexOf('Assets/plus.svg')!==-1){
+        imageInput.src=imageInput.src.replace('plus.svg','minus.svg');
+    } else{
+        imageInput.src=imageInput.src.replace('minus.svg','plus.svg');
+    }
+}
+
+document.getElementById('personal-elevated-button')
+    .addEventListener('click', togglePersonalButton);
+
+function togglePersonalButton() {
+    let imageInput = document.getElementById('personal-elevated-button');
+    if (imageInput.src.indexOf('Assets/plus.svg')!==-1){
+        imageInput.src=imageInput.src.replace('plus.svg','minus.svg');
+    } else{
+        imageInput.src=imageInput.src.replace('minus.svg','plus.svg');
+    }
+}
+
+/* Navigation selection */
